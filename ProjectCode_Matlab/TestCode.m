5912482c36cbd2b290f6f83ee00ceb4c1cbd6b3d
Karan <s3487995@student.rmit.edu.au>
2018-06-19 12:43:42 +1000

    working on scan matching
    using Robotics System Toolbox

--- ProjectCode_Matlab/TestCode.m
+++ ProjectCode_Matlab/TestCode.m
@@ -3,30 +3,46 @@ file = 'room_walk_horseshoe.csv';
 %file = 'test_1.csv';
 
 fid = fopen( file, 'r' ); 
-dist = textscan( fid, '%*d%*d%d%*d', 'Delimiter',',', 'Headerlines',1 );
+range = textscan( fid, '%*d%*d%d%*d', 'Delimiter',',', 'Headerlines',1 );
 fclose( fid ); 
 
 fid = fopen( file, 'r' ); 
-ang = textscan( fid, '%*d%d%*d%*d', 'Delimiter',',', 'Headerlines',1 );
+angle = textscan( fid, '%*d%d%*d%*d', 'Delimiter',',', 'Headerlines',1 );
 fclose( fid ); 
 
-dist = cell2mat(dist);
-ang = cell2mat(ang);
-dist = double(dist);
-ang = double(ang);
-    
-x = cos(ang*(pi/180));
-x = dist.*x;
-y = sin(ang*(pi/180));
-y = dist.*y;
+range = cell2mat(range);
+angle = cell2mat(angle);
+range = double(range);
+angle = double(angle);
+angle = angle * (pi/180);
 
-scatter(x,y,5,'filled');
-ax = gca;
-ax.Title.String = 'Lidar Scan of Room';
-ax.XLim = [-500,500];
-ax.YLim = [-500,500];
-ax.XAxisLocation = 'origin';
-ax.YAxisLocation = 'origin';
-ax.XGrid = 'on';
-ax.YGrid = 'on';
-ax.Box = 'on';
+%x = cos(angle*(pi/180));
+%x = range.*x;
+%y = sin(angle*(pi/180));
+%y = range.*y;
+
+%scatter(x,y,5,'filled');
+%ax = gca;
+%ax.Title.String = 'Lidar Scan of Room';
+%ax.XLim = [-500,500];
+%ax.YLim = [-500,500];
+%ax.XAxisLocation = 'origin';
+%ax.YAxisLocation = 'origin';
+%ax.XGrid = 'on';
+%ax.YGrid = 'on';
+%ax.Box = 'on';
+
+%% ROBOTICS TOOLBOX
+scans = lidarScan(range,angle);
+plot(scans);
+
+
+%map = robotics.OccupancyGrid(15, 15, 20);
+%map.GridLocationInWorld = [-7.5 -7.5];
+
+%numScans = numel(laserMsg);
+%initialPose = [0 0 0];
+%poseList = zeros(numScans,3);
+%poseList(1,:) = initialPose;
+%transform = initialPose;
+
